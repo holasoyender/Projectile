3d8cd29e2d2756820f943425f700c19a15da3680
holasoyender <javier.projectzafire@gmail.com>
2022-06-22 20:16:59 +0200

    Compilation date && attribution

--- src/logger.rs
+++ src/logger.rs
@@ -1,10 +1,11 @@
 use ansi_term::Colour;
 use crate::config_color;
+use build_time::build_time_local;
 
 pub fn banner() {
     if config_color() {
             println!(r#"{}
-Versión: {}. Compilado en: {}.
+Versión: {}. Compilado en: {}. Creado por: {}.
 "#, Colour::Green.paint(r#"
     ____               _           __  _ __
    / __ \_________    (_)__  _____/ /_(_) /__
@@ -12,7 +13,7 @@ Versión: {}. Compilado en: {}.
  / ____/ /  / /_/ / / /  __/ /__/ /_/ / /  __/
 /_/   /_/   \____/_/ /\___/\___/\__/_/_/\___/
                 /___/"#)
-                     , Colour::Red.paint(env!("CARGO_PKG_VERSION")), Colour::Red.paint("Unknown"));
+                     , Colour::Red.paint(env!("CARGO_PKG_VERSION")), Colour::Red.paint(build_time_local!("%Y/%m/%d %H:%M:%S")), Colour::Red.paint("holasoyender"));
         } else {
             println!(r#"
     ____               _           __  _ __
@@ -21,8 +22,8 @@ Versión: {}. Compilado en: {}.
  / ____/ /  / /_/ / / /  __/ /__/ /_/ / /  __/
 /_/   /_/   \____/_/ /\___/\___/\__/_/_/\___/
                 /___/
-Versión: {}. Compilado en: {}.
-"#, env!("CARGO_PKG_VERSION"), "Unknown");
+Versión: {}. Compilado en: {}. Creado por: holasoyender.
+"#, env!("CARGO_PKG_VERSION"), build_time_local!("%Y/%m/%d %H:%M:%S"));
         }
 }
 
