fd430b3a837641800afc8ec98da21750a11841cf
holasoyender <javier.projectzafire@gmail.com>
2022-06-22 15:14:49 +0200

    First-load fixes

--- src/cfg.rs
+++ src/cfg.rs
@@ -31,5 +31,7 @@ pub fn get_root_path() -> String {
 pub fn get_default_config() -> String {
     let config_dir = dirs::config_dir();
     let config_path = config_dir.unwrap().join("Projectile");
-    return format!(r#"project_path: "{}"\ncolor: true "#, config_path.to_str().unwrap().to_string().replace("\\", "\\\\")+"\\\\projects");
+    return format!(r#"
+project_path: "{}"
+color: true "#, config_path.to_str().unwrap().to_string().replace("\\", "\\\\")+"\\\\projects");
 }
--- src/installer.rs
+++ src/installer.rs
@@ -1,6 +1,14 @@
 use crate::cfg;
 
 pub fn install() -> bool {
+
+    if !cfg::config_exists() {
+        //create all folders of get_root_path()
+        let root_path = cfg::get_root_path();
+        if !std::path::Path::new(&root_path).exists() {
+            std::fs::create_dir_all(&root_path).unwrap();
+        }
+    }
     //Copy itself to the folder
     let config_path = cfg::get_root_path();
     //Get the exe path
