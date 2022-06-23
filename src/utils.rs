c75e8159e30189855b6a5815c4d4cd4be27822e0
holasoyender <javier.projectzafire@gmail.com>
2022-06-23 18:38:11 +0200

    Skip typecheck on error

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,12 @@
+use std::fs;
 use crate::logger;
 
 pub fn copy_folder_content(folder_path: &str) -> bool {
-    let folder_path_is_folder = std::fs::metadata(folder_path).unwrap().is_dir();
+    let folder_path_is_folder = fs::metadata(folder_path).unwrap().is_dir();
     return if folder_path_is_folder {
-        let folder_content = std::fs::read_dir(folder_path).unwrap();
+        let folder_content = fs::read_dir(folder_path).unwrap();
         let folder_name = folder_path.split("\\").last().unwrap();
-        let create_folder_action = std::fs::create_dir(format!("./{}", folder_name));
+        let create_folder_action = fs::create_dir(format!("./{}", folder_name));
         match create_folder_action {
             Ok(_) => {
                 logger::info(format!("Creado el directorio {}", folder_name).as_str());
@@ -13,7 +14,7 @@ pub fn copy_folder_content(folder_path: &str) -> bool {
                     let file = file.unwrap();
                     let file_path = file.path();
                     let file_name = file_path.to_str().unwrap().split("\\").last().unwrap();
-                    let copy_action = std::fs::copy(file_path.to_str().unwrap(), format!("./{}/{}", folder_name, file_name.clone()));
+                    let copy_action = fs::copy(file_path.to_str().unwrap(), format!("./{}/{}", folder_name, file_name.clone()));
                     match copy_action {
                         Ok(_) => {
                             logger::info(format!("Copiado {}\\{}", folder_name, file_name).as_str());
@@ -40,11 +41,11 @@ pub fn copy_folder_content(folder_path: &str) -> bool {
 }
 
 pub fn copy_folder_content_with_vars(folder_path: &str, vars_setters: &Vec<&str>, vars_values: &Vec<String>) -> bool {
-    let folder_path_is_folder = std::fs::metadata(folder_path).unwrap().is_dir();
+    let folder_path_is_folder = fs::metadata(folder_path).unwrap().is_dir();
     return if folder_path_is_folder {
-        let folder_content = std::fs::read_dir(folder_path).unwrap();
+        let folder_content = fs::read_dir(folder_path).unwrap();
         let folder_name = folder_path.split("\\").last().unwrap();
-        let create_folder_action = std::fs::create_dir(format!("./{}", folder_name));
+        let create_folder_action = fs::create_dir(format!("./{}", folder_name));
         match create_folder_action {
             Ok(_) => {
                 logger::info(format!("Creado el directorio {}", folder_name).as_str());
@@ -52,7 +53,7 @@ pub fn copy_folder_content_with_vars(folder_path: &str, vars_setters: &Vec<&str>
                      let file = file.unwrap();
                      let file_path = file.path();
                      let file_name = file_path.to_str().unwrap().split("\\").last().unwrap();
-                     let file_content = std::fs::read_to_string(file_path.to_str().unwrap());
+                     let file_content = fs::read_to_string(file_path.to_str().unwrap());
                      match file_content {
                          Ok(mut content) => {
                              let mut name = String::new();
@@ -63,15 +64,24 @@ pub fn copy_folder_content_with_vars(folder_path: &str, vars_setters: &Vec<&str>
                                  content = content.replace(format!(r#"{{{{{}}}}}"#, setter).as_str(), value.as_str());
                                  name = file_name.replace(format!(r#"{{{{{}}}}}"#, setter).as_str(),value.as_str());
                              }
-                             std::fs::write(format!("./{}/{}", folder_name, name), content).unwrap();
+                             fs::write(format!("./{}/{}", folder_name, name), content).unwrap();
                              logger::info(format!("Copiado {}", name).as_str());
                          },
                          Err(_) => {
                              if copy_folder_content_with_vars(format!("{}/{}", folder_path, file_name).as_str(), vars_setters, vars_values) {
                                  logger::info(format!("Copiado {}", file_name).as_str());
                              } else {
-                                 logger::error(format!("Error al copiar el archivo {}", file_name).as_str());
-                                 return false;
+                                 let copy_action = fs::copy(file_path.clone(), format!("./{}/{}", folder_name, file_name));
+                                 match copy_action {
+                                     Ok(_) => {
+                                         logger::info(format!("Copiado {}", file_name).as_str());
+                                         return true
+                                     },
+                                     Err(_) => {
+                                         logger::error(format!("Error al copiar el archivo {}", file_name).as_str());
+                                         return false;
+                                     }
+                                 }
                              }
                          }
                      }
@@ -85,6 +95,6 @@ pub fn copy_folder_content_with_vars(folder_path: &str, vars_setters: &Vec<&str>
 
         true
     } else {
-        false
+       false
     }
 }
